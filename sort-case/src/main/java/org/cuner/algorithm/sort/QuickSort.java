e819f8d09cc914b8fdc36c9812b326c9b4475f91
houan <houan@meili-inc.com>
2018-08-23 11:12:08 +0800

    add quick sort

---
+++ sort-case/src/main/java/org/cuner/algorithm/sort/QuickSort.java
@@ -0,0 +1,29 @@
+package org.cuner.algorithm.sort;
+
+/**
+ * Created by houan on 18/8/23.
+ */
+public class QuickSort {
+
+    /**
+     *
+     * @param arr
+     * @param left
+     * @param right
+     */
+    public static void quick_sort_swap(int[] arr, int left, int right) {
+        if (arr == null || arr.length == 0) {
+            return;
+        }
+        if (left >= right) {
+            return;
+        }
+
+
+
+    }
+
+    public static void quick_sort_dig() {
+
+    }
+}
